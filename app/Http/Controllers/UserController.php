2a14f1be977838fbaabef8e145c82670dd4a691c
STUDENT <astroluxlucis@gmail.com>
2017-01-13 15:08:32 +0800

    Initial Commit | Added lastname and phone | Configure changing of password | Configure usercontroller, account.blade and welcome.blade

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,107 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class UserController extends Controller
+{
+    public function postSignUp(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email|unique:users',
+            'first_name' => 'required|max:120',
+            'last_name' => 'required|max:120',
+            'phone' => 'required|min:7|max:11',
+            'password' => 'required|min:4'
+        ]);
+
+        $email = $request['email'];
+        $first_name = $request['first_name'];
+        $last_name = $request['last_name'];
+        $phone = $request['phone'];
+        $password = bcrypt($request['password']);
+
+        $user = new User();
+        $user->email = $email;
+        $user->first_name = $first_name;
+        $user->last_name = $last_name;
+        $user->phone = $phone;
+        $user->password = $password;
+
+        $user->save();
+
+        Auth::login($user);
+
+        return redirect()->route('dashboard');
+    }
+
+    public function postSignIn(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
+            return redirect()->route('dashboard');
+        }
+        return redirect()->back();
+    }
+
+    public function getLogout()
+    {
+        Auth::logout();
+        return redirect()->route('home');
+    }
+
+    public function getAccount()
+    {
+        return view('account', ['user' => Auth::user()]);
+    }
+
+    public function postSaveAccount(Request $request)
+    {
+        $this->validate($request, [
+           'first_name' => 'required|max:120'
+        ]);
+
+        $user = Auth::user();
+        $old_name = $user->first_name;
+        $nw_password = $request['password'];
+        $user->first_name = $request['first_name'];
+        $user->last_name = $request['last_name'];
+        $user->phone = $request['phone'];
+        if(!empty($nw_password))
+        {
+            $user->password = bcrypt($nw_password);
+        }
+        $user->update();
+        $file = $request->file('image');
+        $filename = $request['first_name'] . '-' . $user->id . '.jpg';
+        $old_filename = $old_name . '-' . $user->id . '.jpg';
+        $update = false;
+        if (Storage::disk('local')->has($old_filename)) {
+            $old_file = Storage::disk('local')->get($old_filename);
+            Storage::disk('local')->put($filename, $old_file);
+            $update = true;
+        }
+        if ($file) {
+            Storage::disk('local')->put($filename, File::get($file));
+        }
+        if ($update && $old_filename !== $filename) {
+            Storage::delete($old_filename);
+        }
+        return redirect()->route('account');
+    }
+
+    public function getUserImage($filename)
+    {
+        $file = Storage::disk('local')->get($filename);
+        return new Response($file, 200);
+    }
+}
